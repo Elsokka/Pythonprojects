8df38ac4e061e10d69642b4289bd4ff00ade6aff
Sokka <felipeortegad@gmail.com>
2022-07-23 19:56:08 -0500

    Cambio de tipo de variable

--- notapy.py
+++ notapy.py
@@ -8,6 +8,14 @@ num_2 = 4
 # Tipos de datos : Enteros, Flotantes, Strings y Booleanos
 # Con estos tipos de datos podemos hacer operaciones.
 # Concatenar, multiplicar 
+
 nombre = "Facundo"
+
+#Cambiar de tipo de dato
+
+num_1 = int(input("Escribe un numero :"))
+nombre = str(num_1)
+print(nombre)
+
 
 
