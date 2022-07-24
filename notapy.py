8dd80cb4c35c9d217e211eebf7e7e9e922f70986
Sokka <felipeortegad@gmail.com>
2022-07-23 19:33:27 -0500

    Se dejan solo los archivos

---
+++ notapy.py
@@ -0,0 +1,13 @@
+# Notas de python 
+
+# Variables : Contenedor de objetos
+
+num_1 = 3
+num_2 = 4
+
+# Tipos de datos : Enteros, Flotantes, Strings y Booleanos
+# Con estos tipos de datos podemos hacer operaciones.
+# Concatenar, multiplicar 
+nombre = "Facundo"
+
+
