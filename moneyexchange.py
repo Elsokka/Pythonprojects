c2c83f4fe01fa15f37fe0928b42818c9f93d33fe
Sokka <felipeortegad@gmail.com>
2022-07-24 13:19:03 -0500

    Nuevas clases y codigo

--- moneyexchange.py
+++ moneyexchange.py
@@ -1,14 +1,32 @@
-#Variables
+def conversor(tipo_dato, valor_dolar):
+    pesos = input("Cuantos pesos " + tipo_dato + " tienes ?")
+    pesos = float(pesos)
+    dolares = (pesos / valor_dolar)
+    dolares =round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes en total $ " + dolares + " dolares")
 
-pesos = float(input(" Cuantos pesos colombianos tienes ?:  "))
-valor_dolar = 3875
+Menu = """""
+Bienvenido al conversor de monedas
 
-# Operacion
+1 - Pesos colombianos
+2 - Pesos argentinos
+3 - Pesos chilenos
 
-dolares = (pesos / valor_dolar)
-dolares =round(dolares, 2)
-dolares = str(dolares)
-print("Tienes en total $ " + dolares + " dolares")
+Elige una opcion: 
+"""
+pais = int(input(Menu))
+
+#definicion de conversion
+
+if pais == 1:
+    conversor("Colombianos", 3840)
+elif pais == 2:
+    conversor("Argentinos", 60)
+elif pais == 3:
+    conversor("Chilenos", 24)
+else:
+    print("El valor ingresado no corresponde a un pais del menu")
 
 
 
--- notapy.py
+++ notapy.py
@@ -1,15 +1,13 @@
-# Notas de python 
 
 # Variables : Contenedor de objetos
 
 num_1 = 3
 num_2 = 4
+nombre = "Facundo"
 
 # Tipos de datos : Enteros, Flotantes, Strings y Booleanos
 # Con estos tipos de datos podemos hacer operaciones.
-# Concatenar, multiplicar 
 
-nombre = "Facundo"
 
 #Cambiar de tipo de dato
 
@@ -17,13 +15,60 @@ num_1 = int(input("Escribe un numero :"))
 nombre = str(num_1)
 print(nombre)
 
-# and para comparar si dos valores son verdaderos.
-# -or para comparar si dos valores son falsos.
-# -not para invertir el valor booleano.
-# -== Compara dos valores y te dice si son iguales o no.
-# -!= Compara dos valores y te dice sin son diferentes o no.
-# -> Compara si es mayor que otro valor.
-# -> Compara si es menor que otro valor.
-# ->= igual o mayor que el valor a comparar.
-# -<= igual o menor que el valor a comparar.
+# Operadores logicos
 
+# and : para comparar si dos valores son verdaderos.
+# or  : para comparar si dos valores son falsos.
+# not : para invertir el valor booleano.
+# ==  : Compara dos valores y te dice si son iguales o no.
+# !=  : Compara dos valores y te dice sin son diferentes o no.
+# >   : Compara si es mayor que otro valor.
+# >   : Compara si es menor que otro valor.
+# >=  : igual o mayor que el valor a comparar.
+# <=  : igual o menor que el valor a comparar.
+#pass : Sirve para saltar esa linea de codigo
+#Triples comillas:  deja escribir una cada mas larga de caractares
+
+# Funciones : Definimos una funciones que se repite y luego la invocamos.
+
+
+def imprimir_mensaje():
+    print("Mensaje especial: ")
+    print("Estoy aprendiendo a usar funciones !")
+
+imprimir_mensaje()
+
+
+# Estas pueden tener parametros
+# La funcion se ejecuta teniendo en cuenta el parametro
+# y el valor que le damos, asi como el impacto que tiene dentro de la funcion
+
+def conversacion(mensaje):
+    print("Hola")
+    print(mensaje)
+
+opcion = int(input("Elige una opcion (1, 2, 3 ): "))
+
+if opcion == 1:
+    conversacion("Elegiste opcion 1")
+elif opcion == 2:
+    conversacion("Elegiste opcion 2")
+elif opcion== 3:
+    conversacion("Elegiste opcion 2")
+else:
+    print("El valor ingresado no corresponde a un numero valido")
+
+
+# Si queremos aprovecahr el resultado de ejecutar la función debemos utilizar 
+# el objeto "return"
+
+def suma(a, b):
+    print("Se suman dos numeros")
+    resultado = a + b
+    return resultado
+
+sumatoria = suma(1, 4)
+print(sumatoria)
+
+#Esto lo que hace es ejecutar la funcion con los parametros dados, guardar el valor 
+#producto de la ejecucion de la funcion y guardarlo en otro objeto 
