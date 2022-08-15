df38ba2317294bf230b157b5d3887146b20a4cf4
Sokka <felipeortegad@gmail.com>
2022-08-15 09:19:48 -0500

    Reto de leap year resuelto

--- HackerRank.py
+++ HackerRank.py
@@ -1,8 +1,12 @@
-def run():
-     n = int(input())
-     for i in range(n):
-        print(i**2)
-    
+def is_leap(year):
+    leap = False
+    if year%4 == 0 and (year%100 != 0 or year%400 == 0):   
+        leap = True
+    return leap
+#Evalua solo las condiciones en que verdadero, es decir para que sea leap year, 
+# debe ser divisible entre 4, no ser divisible entre 100 y ser divisible entre 400.
+
 
 if __name__ == "__main__":
-    run()
+    year = int(input())
+    print(is_leap(year))
