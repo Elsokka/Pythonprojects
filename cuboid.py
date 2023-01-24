b1a16a4612b822df68055e23e28ac9cac826425f
Pipe Juan <felipeortegad@gmail.com>
2023-01-23 22:14:50 -0500

    Reto hackerrank Python

---
+++ cuboid.py
@@ -0,0 +1,12 @@
+def permutacion(x,y,z,n): #Nos ayudara a encontrar las permutaciones posibles de i , j, k teniendo en cuenta X, Y, Z co la restricción n.
+
+
+
+if __name__ == "__main__":
+    x = int(input())
+    y = int(input())
+    z = int(input())
+    n = int(input())
+    
+
+lista_completa = []
