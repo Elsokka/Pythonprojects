59b053cc1813025c5775482270818625feafa112
Pipe Juan <felipeortegad@gmail.com>
2024-09-03 07:52:19 -0500

    Empezamos a trabajar en nuestro poryecto y repasamos List topics

---
+++ healthapp.py
@@ -0,0 +1,4 @@
+# Proyecto de aprendizaje de una app de salud informativa
+
+dni = input('¿Cual es su numero de identificación?:')
+print(dni)
--- reutilizable.py
+++ reutilizable.py
@@ -1,2 +1,8 @@
-a = 'Felipe'
-print (a)
+a = []
+for a in a:
+    print(a)
+
+# Print Formatting With Strings 
+value = 3000
+print('Usá tus ${} créditos para productos de farmacia y bienestar. ¡Rappi te ayuda! Aplican T&C.'.format(value))
+print(f'Usá tus ${value} créditos para productos de farmacia y bienestar. ¡Rappi te ayuda! Aplican T&C.')
