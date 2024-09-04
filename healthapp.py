19605863d0f27ffb60544cd6878681789d17b88b
Pipe Juan <felipeortegad@gmail.com>
2024-09-04 07:51:42 -0500

    Clase extra de python

--- healthapp.py
+++ healthapp.py
@@ -1,4 +1,7 @@
 # Proyecto de aprendizaje de una app de salud informativa
 
-dni = input('¿Cual es su numero de identificación?:')
-print(dni)
+my_diction = {'name':'Felipe','age': 25 , 'dni':'1051420586'}
+print(my_diction['name'])
+
+#dni = input('¿Cual es su numero de identificación?:')
+#print(dni)
